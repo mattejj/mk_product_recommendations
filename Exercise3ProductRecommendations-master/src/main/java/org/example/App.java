df93a3a0c89449b6423655a35a6d30c1c6121af6
mattejj <44164827+mattejj@users.noreply.github.com>
2025-02-18 14:22:56 +0100

    Parser done
    lombok, parser

--- Exercise3ProductRecommendations-master/src/main/java/org/example/App.java
+++ Exercise3ProductRecommendations-master/src/main/java/org/example/App.java
@@ -1,13 +1,97 @@
 package org.example;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Hello world!
- *
+ * Product recommendation
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        /*
+        1. get name of the file (and location?)
+        1.a. get threshold
+        2. read file, ignore lines that don't start with number (id of product)
+            -- #soId|ppnkId|soCountCode|name|mobile|email|productList
+        3. save products as class object
+        ... combine products based on id, and num of sold products
+         */
+
+        // number of common purchases
+        int threshold = 1;
+
+        List<Order> orderList = new ArrayList<>();
+
+        // get input
+        String line = "";
+        try {
+            String filename = "example1_in_data.txt";
+            String fileLocation = new java.io.File(".").getCanonicalPath() + "\\Exercise3ProductRecommendations-master\\test_files\\";
+
+            BufferedReader reader = new BufferedReader(new FileReader(fileLocation + filename));
+            while ((line = reader.readLine()) != null) {
+                // is order
+                Order orderNew = new Order();
+
+
+                if (Character.isDigit(line.charAt(0))) {
+                    // parse product in class object
+                    int nextI;
+
+                    for (int counter = 0; counter < 7; counter++) {
+                        nextI = line.indexOf("|");
+                        String dataString = "";
+                        if(nextI>0) {
+                            dataString = line.substring(0, nextI);
+                            line = line.substring(nextI + 1);
+                        }
+
+                        int dataInt = 0;
+                        if (counter <= 1) {
+                            dataInt = Integer.valueOf(dataString);
+                        }
+
+                        switch (counter) {
+                            case 0:
+                                //soId
+                                orderNew.setSoId(dataInt);
+                                break;
+                            case 1:
+                                //ppnkId
+                                orderNew.setPpnkId(dataInt);
+                                break;
+                            case 2:
+                                //soCountCode
+                                orderNew.setSoCountCode(dataString);
+                                break;
+                            case 3:
+                                //name
+                                orderNew.setName(dataString);
+                                break;
+                            case 4:
+                                //mobile
+                                orderNew.setMobile(dataString);
+                                break;
+                            case 5:
+                                //email
+                                orderNew.setEmail(dataString);
+                                break;
+                            case 6:
+                                //productList
+                                orderNew.SplitProductList(line);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
---
+++ Exercise3ProductRecommendations-master/src/main/java/org/example/Order.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Order {
+
+    //Order Id
+    protected int soId;
+    //Partner Id
+    public int ppnkId;
+    //Order number
+    public String soCountCode;
+    public String name;
+    public String mobile;
+    public String email;
+    public List<String> productList = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "soId=" + soId +
+                ", ppnkId=" + ppnkId +
+                ", soCountCode='" + soCountCode + '\'' +
+                ", name='" + name + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", email='" + email + '\'' +
+                ", productList=" + productList +
+                '}';
+    }
+
+    public void SplitProductList(String s) {
+        do {
+            // take string to "," or i absent full string
+
+            int strLength = s.length();
+            if (strLength == 0) {
+                return;
+            }
+
+            int nextDash = s.indexOf("|");
+
+            if (nextDash > 0) {
+                String tempS = s.substring(0, nextDash);
+                s = s.substring(nextDash + 1);
+                productList.add(tempS);
+
+            } else {
+                productList.add(s);
+                return;
+            }
+        }
+        while (true);
+    }
+}
